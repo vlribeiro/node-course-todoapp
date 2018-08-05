543400fbf7d764e48e17645a741894b2d4e1e34f
Vinicius <vl.ribeiro3@gmail.com>
2018-08-05 10:30:53 -0300

    chore(tests): Add GET /todos basic test
    Add initial seed data configuration and GET /todos test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,18 @@ const request = require(`supertest`)
 const {app} = require(`./../server`)
 const {Todo} = require(`./../models/todo`)
 
+const todosSeed = [{
+    text: `First test todo`
+}, {
+    text: `Second test todo`
+}]
+
 beforeEach(done => {
     Todo.remove({})
-    .then(() => done())
+    .then(() => {
+        return Todo.insertMany(todosSeed)
+        .then(() => done())
+    })
 })
 
 describe(`POST /todos`, () => {
@@ -24,7 +33,7 @@ describe(`POST /todos`, () => {
             if (err)
                 return done(err)
             
-            Todo.find()
+            Todo.find({text})
             .then(todos => {
                 expect(todos.length).toBe(1)
                 expect(todos[0].text).toBe(text)
@@ -45,10 +54,22 @@ describe(`POST /todos`, () => {
             
             Todo.find()
             .then(todos => {
-                expect(todos.length).toBe(0)
+                expect(todos.length).toBe(todosSeed.length)
                 done()
             })
             .catch(e => done(e))
         })
+    })
+})
+
+describe(`GET /todos`, () => {
+    it (`should get all todos`, done => {
+        request(app)
+        .get(`/todos`)
+        .expect(200)
+        .expect(res => {
+            expect(res.body.todos.length).toBe(todosSeed.length)
+        })
+        .end(done)
     })
 })
