ee3bfd16fdaa245b6452adf524bd160c725ee107
Vinicius <vl.ribeiro3@gmail.com>
2018-08-11 18:40:20 -0300

    feat(server): Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,23 @@ UserSchema.methods.generateAuthToken = function() {
     return user.save().then(() => token)
 }
 
+UserSchema.statics.findByToken = function(token) {
+    const User = this
+    let decoded
+
+    try {
+        decoded = jwt.verify(token, `segredooo`)
+    } catch (e) {
+        return Promise.reject()
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': `auth`
+    })
+}
+
 const User = mongoose.model(`User`, UserSchema)
 
 module.exports = {User}
